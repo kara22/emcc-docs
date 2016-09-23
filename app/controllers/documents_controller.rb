59ce4e026a945766ba21ae30da2b19942d91166f
Kara Diaby <kdiaby.pro@gmail.com>
2016-09-23 23:00:58 +0200

    Initial commit

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -2,10 +2,13 @@ class DocumentsController < ApplicationController
 
 before_action :find_document, only: [:show, :edit, :update, :destroy]
 
+
   def index
-
+    if params[:category]
+      @documents = Document.where(category: params[:category])
+    else
     @documents = Document.all
-
+    end
   end
 
   def show
@@ -13,11 +16,11 @@ before_action :find_document, only: [:show, :edit, :update, :destroy]
 
   def new
     @document = Document.new
-
   end
 
   def create
     @document = Document.new(document_params)
+    @document.user = current_user
     if @document.save
         redirect_to documents_path
     else
@@ -29,29 +32,38 @@ before_action :find_document, only: [:show, :edit, :update, :destroy]
   end
 
   def update
-    if @document.update(document_params)
-        redirect_to documents_path
-      else
-        render :edit
-      end
+      if @document.user == current_user
+      @document.update(document_params)
+      redirect_to documents_path
+    else
+       flash[:alert] = "Edition impossible, ce produit n'est pas le vôtre..."
+       redirect_to documents_path
+    end
   end
 
   def destroy
+    if @document.user == current_user
      @document.destroy
      redirect_to documents_path
-      end
+    else
+      flash[:alert] = "Suppression impossible, ce produit n'est pas le vôtre..."
+      redirect_to documents_path
+    end
 
-private
+  end
+
 
 private
 
   def document_params
-    params.require(:document).permit(:name, :tagline, :category)
+    params.require(:document).permit(:name, :tagline, :category, :photo)
   end
 
   def find_document
   @document = Document.find(params[:id])
   end
+
+
 end
 
 
--- app/models/document.rb
+++ app/models/document.rb
@@ -1,7 +1,13 @@
 class Document < ApplicationRecord
 
+
+  has_attachment :photo
+
+  belongs_to :user
+
+  validates :user, presence: true
   validates :name, presence: true, uniqueness: true
   validates :tagline, presence: true , uniqueness: true
-validates :category, inclusion: { in: %w(fiche_technique template suivi_materiel_assurances suivi_materiel_dma rapport_hebdomadaire suivi_personnel_export ),
-    message: "%{value} is not a valid category" }
+  validates :category, inclusion: { in: %w(fiche_technique template suivi_materiel_assurances suivi_materiel_dma rapport_hebdomadaire suivi_personnel_export ),
+    message: "%{value} N'est pas une catégorie valide" }
 end
