29fdb4d159f797a0a33a913ec4cac10cf1c3516a
Kara Diaby <kdiaby.pro@gmail.com>
2016-09-25 17:15:34 +0200

    description non required

--- app/models/document.rb
+++ app/models/document.rb
@@ -7,7 +7,6 @@ class Document < ApplicationRecord
 
   validates :user, presence: true
   validates :name, presence: true, uniqueness: true
-  validates :tagline, presence: true
   validates :category, inclusion: { in: %w(fiche_technique template suivi_materiel_assurances suivi_materiel_dma rapport_hebdomadaire suivi_personnel_export ),
     message: "%{value} N'est pas une catégorie valide" }
   validates :url, presence: true, uniqueness: true
