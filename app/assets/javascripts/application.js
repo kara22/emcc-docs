ce25829fd4bbaf838a60f4fc35017fc5c95f60c5
Kara Diaby <kdiaby.pro@gmail.com>
2016-09-25 17:58:40 +0200

    add javascript to down on the page + remove of certains elements

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,3 +5,19 @@
 //= require cloudinary/jquery.cloudinary
 //= require attachinary
 //= require_tree .
+
+
+$(document).ready(function() {
+
+$(".slide-section").click(function(e){
+
+    var linkHref = $(this).attr("href");
+
+    $("html, body").animate({
+      scrollTop : $(linkHref).offset().top
+    }, 1000);
+
+    e.preventDefault();
+  });
+
+});
