bbc0fae151ef0fd201d8a466b96b53c66aaaf0e2
Kara Diaby <kdiaby.pro@gmail.com>
2016-09-27 23:24:43 +0200

    add the correction to download xls pdf or image

--- app/models/document.rb
+++ app/models/document.rb
@@ -7,6 +7,7 @@ class Document < ApplicationRecord
 
   validates :user, presence: true
   validates :name, presence: true, uniqueness: true
+  validates :tagline, presence: true
   validates :category, inclusion: { in: %w(fiche_technique template suivi_materiel_assurances suivi_materiel_dma rapport_hebdomadaire suivi_personnel_export infos_QHSE),
     message: "%{value} N'est pas une catégorie valide" }
 end
