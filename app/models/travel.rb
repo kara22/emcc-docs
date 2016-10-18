edf824aa1c6eca5fb1ea3d73795f42ffc24dd4de
Kara Diaby <kdiaby.pro@gmail.com>
2016-10-18 23:06:07 +0200

    remove Jean Dupont

--- app/models/travel.rb
+++ app/models/travel.rb
@@ -9,7 +9,7 @@ class Travel < ApplicationRecord
   validates :reason, presence: true, inclusion: { in: %w(Congés_payés_Expatriation Congés_payés_Caisse RTT Jour_de_Voyage_pointés_VO Récupération Jours_fériés_locaux Conventionnelle Congé_sans_solde ),
     message: "%{value} veuillez renseigner le motif de votre absence" }
 
-  validates :manager_name, presence: true, inclusion: { in: %w(Olivier_Assemat Gervais_Bouchard Antoine_Djondo Marc_Fraillon Henri_Seynave Jean_Michel_Chevrier Jean_Dupont),
+  validates :manager_name, presence: true, inclusion: { in: %w(Olivier_Assemat Gervais_Bouchard Antoine_Djondo Marc_Fraillon Henri_Seynave Jean_Michel_Chevrier),
     message: "%{value} veuillez renseigner le nom de votre manager" }
 
   validates :site, presence: true, inclusion: {in: %w(Haiti Kingston_Jamaique Abidjan_Côte_Ivoire Kamsar_Guinée Lomé_Togo Cotonou_Bénin),
